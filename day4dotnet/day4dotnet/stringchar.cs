eeb81a65e281ce06140f9fb996e28db81b69665f
Sriharshini Ganti <sriharshinig@infinite.com>
2022-10-05 22:48:33 +0530

    assignment4

---
+++ day4dotnet/day4dotnet/stringchar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4dotnet
+
+{
+    class stringchar
+
+    { 
+           public static void Main()
+            {
+                string str;
+                char ch;
+                Console.Write("Enter the string : ");
+                str = Console.ReadLine();
+                Console.Write("Enter the Character");
+                ch = Convert.ToChar(Console.ReadLine());
+                int freq = 0;
+            foreach (char c in str)
+            {
+                if (c == ch)
+                {
+                    freq++;
+                }
+            }
+            Console.WriteLine(freq);
+            Console.ReadLine();
+            }
+        
+
+    }
+}
